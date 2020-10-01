05401aad3246f259e9dda177614da52a106ab8bc
smkrizan <smkrizan@gmail.com>
2020-10-01 14:18:44 -0500

    scheduled entry collected w row id

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,27 @@
+var schedule = {};
 
-// delegating click listener to parent div element for child textarea element
-$(".form-group").on("click", "textarea", function () {
-    console.log("the schedule entry textarea was clicked");
-    // get inner text content of current element and remove leading/trailing whitespace
-    var entry = $(this).text().trim();
-    console.log(entry);
-
+// click event listener for save buttons
+$(".saveBtn").on("click", function () {
 })
+
+// delegating to parent div element for child textarea element
+$(".form-group").on("blur", "textarea", function () {
+    // get textarea's current value/text and remove leading/trailing whitespace
+    var schedEntry = $(this).val().trim();
+    console.log(schedEntry)
+
+    // get the row div's id attribute
+    var rowId = $(this)
+    .closest(".row")
+    .attr("id");
+    console.log(rowId)
+})
+
+// // placeholders for task entries
+// schedule[schedEntry][rowId].val = schedEntry;
+// console.log(schedule)
+// console.log(schedule[schedEntry])
+// console.log(schedule[schedEntry].val)
+// console.log(schedule[schedEntry][rowId])
+// console.log
+// // saveSched();
