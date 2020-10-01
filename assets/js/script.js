ecdbbce1bacf177cd7509aa0ee0b02af3adebdb0
smkrizan <smkrizan@gmail.com>
2020-10-01 17:39:10 -0500

    working on save to array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,45 @@
+// GLOBAL VARIABLES
 // variable to hold array of schedule entries
 var schedule = {};
-
 // get today's date
 var today = moment().format("dddd, MMMM Do YYYY");
-console.log(today)
 
 // place the day/date at top of page
-var displayDate = function (today) {   
-    // create element to hold date
-    var dateH5 = $("<h5>")
-        .text(today);
-
-    //append h5 element to parent div on the page
-    $(".div.today").append(dateH5);
+var displayDate = function () {
+    $("#currentDay").text(today);
 }
-console.log(displayDate)
+displayDate()
+
+// load schedule from local storage and parse into array
+var loadSched = function () {
+    schedule = JSON.parse(localStorage.getItem("schedule"));
+
+    // if nothing has been saved to local storage, create a new object to track schedule entries
+    if (!schedule) {
+        schedule = {
+            text: [],
+            time: [],
+            id: [],
+        };
+    }
+
+    // loop through entry-object properties
+    $.each(schedule, function (list, arr) {
+        createEntry(schedule.text, schedule.time, schedule.id);
+    });
+}
+loadSched()
+
+// save schedule entries to local storage
+var saveSched = function () {
+    localStorage.setItem("schedule", JSON.stringify(schedule));
+}
+
+// click event listener for save buttons
+saveEntry = $(".saveBtn").on("click", function () {
+})
+console.log(saveEntry)
+saveSched()
 
 // delegating to parent div element for child textarea element
 $(".form-group").on("blur", "textarea", function () {
@@ -22,31 +47,26 @@ $(".form-group").on("blur", "textarea", function () {
     var schedEntry = $(this).val().trim();
     console.log(schedEntry)
 
-    // get the row div's id attribute
+    // get the parent row div's id attribute
     var rowId = $(this)
-    .closest(".row")
-    .attr("id");
+        .closest(".row")
+        .attr("id");
     console.log(rowId)
+
+    // get the schedule entry position in the list of other entries
+    var index = $(this)
+        .closest(".row")
+        .index()
+    console.log(index)
+
+    // push entry as an array to the object 'schedule'
+    schedule.push({
+        text: schedEntry,
+        time: rowId,
+        id: index,
+    });
 })
-
-// create object for schedule entry
-// // placeholders for task entries
-// schedule[schedEntry][rowId].val = schedEntry;
-// console.log(schedule)
-// console.log(schedule[schedEntry])
-// console.log(schedule[schedEntry].val)
-// console.log(schedule[schedEntry][rowId])
-// console.log
-// // saveSched();
-
-// click event listener for save buttons
-$(".saveBtn").on("click", function () {
-})
-
-// save schedule entries to local storage
-var saveSched = function() {
-    localStorage.setItem("schedule", JSON.stringify(schedule));
-}
+saveSched();
 
 // update schedule to show status of entries
 var auditSched = function () {
