df3099ee95e28159f75154b050da97e98ff44001
smkrizan <smkrizan@gmail.com>
2020-10-03 14:04:26 -0500

    save button updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 // variable to hold array of schedule entries
 var schedule = [];
+console.log(schedule)
 
 // get today's date & place at top of page
 var today = moment().format("dddd, MMMM Do YYYY");
@@ -11,14 +12,17 @@ displayDate()
 // load schedule from local storage and parse into array
 var loadSched = function () {
     schedule = localStorage.getItem("schedule");
+    console.log(schedule)
 
     // if nothing has been saved to local storage, discontinue function
     if (!schedule) {
+        schedule = [];
         return false
     }
 
     // parse string back into an array of objects
     schedule = JSON.parse(schedule);
+    console.log(schedule)
 
     // loop through each saved schedule entry to place text within schedule
     for (var i = 0; i < schedule.length; i++) {
@@ -62,16 +66,8 @@ $(".form-group").on("blur", "textarea", function () {
 var auditSched = function () {
     // establish current time
     currentHour = parseInt(moment().format("H"));
-    minutesNow = parseInt(moment().format("m"));
-    // if (minutesNow > 30) {
-    //     currentHour = currentHour + 1;
-    // }
-    // console.log(currentHour)
 
-    // get time from schedule for comparison
-    var sched = $(".hour");
-
-    // calculate difference between schedule entries and current time; my TA helped me to define the string literanl enabling this solution to the problem
+    // calculate difference between schedule entries and current time; my TA helped me to define the string literal enabling this solution to the problem
     for (i=7; i<19; i++) {
         if (currentHour > i) {
             $(`.t${i}`).addClass("past");
